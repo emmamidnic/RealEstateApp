b659c90e12b1e38c9b5f184457f3b337c3e79775
EgonRasmussen <ecr@eucsyd.dk>
2022-11-17 10:50:58 +0100

    Add project files.

---
+++ RealEstateApp/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using RealEstateApp.Models;
+
+namespace RealEstateApp.Repositories
+{
+    public interface IRepository
+    {
+        List<Agent> GetAgents();
+        List<Property> GetProperties();
+        void SaveProperty(Property property);
+    }
+}
